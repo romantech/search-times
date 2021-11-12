cf7527ecba09cea448d39c34f07daa917488be86
Johan <johan@romantech.net>
2021-11-12 16:37:11 +0900

    :tada: 프로젝트 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+const App = function (): JSX.Element {
+  return (
+    <StyledWrapper>
+      <h1>Hello World</h1>
+    </StyledWrapper>
+  );
+};
+
+const StyledWrapper = styled.section``;
+
+export default App;
