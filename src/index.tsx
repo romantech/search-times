62c6ccd2751a02b0a8de496d0b439c590e3242c5
Johan <johan@romantech.net>
2021-11-14 16:11:59 +0900

    :rocket: 배포 테스트

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import { persistStore } from 'redux-persist';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './modules';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './styles/globalStyle';
 import 'antd/dist/antd.css';
 
@@ -30,8 +29,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
